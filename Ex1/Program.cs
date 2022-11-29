7d2886dcc98206eb9c1ebf3b1df469a06bbc97c6
pr0gmatik <hozianinki@gmail.com>
2022-11-29 21:03:03 +0300

    1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+//456 -> 5
+
+int num = 456;
+int num1 = num % 100;
+int num2 = num1 / 10;
+Console.Write(num2);
+
+// //782 -> 8
+// int num = 782;
+// int num1 = num % 100;
+// int num2 = num1 / 10;
+// Console.Write(num2);
+
+// //918 -> 1
+// int num = 918;
+// int num1 = num % 100;
+// int num2 = num1 / 10;
+// Console.Write(num2);
--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Задача 10 ");
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine(stringNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+
+
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+//int num = new Random().Next(100, 1000);
+//Console.WriteLine("Введите число : ");
+//int num = new Random().Next(100, 1000);
+int num = 456;
+//Console.WriteLine($"сгенерировано случайное число {num}");
+
+// int num1 = num % 100;
+// int num5 = num1 / 10;
+// int num2 = num / 100;
+// int num3 = num / 10;
+// //int num2 = num % 10;
+// //Console.Write(num1);
+// int num4 = num2 - num3;
+// Console.WriteLine(num1);
+// Console.WriteLine(num2);
+// Console.WriteLine(num3);
+// Console.WriteLine(num5);
+// Console.Write(num4);
