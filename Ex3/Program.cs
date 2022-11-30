f9a61b5af5b4bd6d571510eb349cc646450331b9
pr0gmatik <hozianinki@gmail.com>
2022-11-30 19:16:58 +0300

    1

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -1,6 +1,9 @@
 ﻿Console.WriteLine("Введите число от 1 до 7 : ");
 int a = Convert.ToInt32(Console.ReadLine());
-if (a>5){
+if (a > 7){
+    Console.WriteLine($"{a} это число больше 7 ");
+}
+else if (a>5){
     Console.WriteLine($"{a} день недели выходной");
 }
 else {
