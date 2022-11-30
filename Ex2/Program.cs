375b4593b1d10caca9474f9123586386b6cd7b25
pr0gmatik <hozianinki@gmail.com>
2022-11-30 19:00:35 +0300

    1

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,46 +1,29 @@
-﻿// Console.WriteLine("Задача 10 ");
-// Console.Write("Введи трёхзначное число: ");
-// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-// string stringNumber = Convert.ToString(threeDigitNumber);
-// Console.WriteLine(stringNumber);
-// Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
-
-
-//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
-//456 -> 5
-//782 -> 8
-//918 -> 1
-
-int num = 5645;
-int num1 = num / 100;
-if (num > 10)
-//if (num1 == 0)
-{
-     int num2 = num1 % 10;
-    Console.WriteLine($"В числе 645, третье число {num2}");
-    //Console.Write("В данном числе, третьей цифры нет");
-
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//2679 -> 6
+int num = 645;
+if (num/100 < 1){
+    Console.WriteLine($"В числе {num}, третьей цифры нет");
+}
+else {
+Console.WriteLine($"В числе {num}, третье число {(num/100)%10}");
 }
 
-//else if (num1 > 10) ;
-else if (num1 == 0)
-{
-    int num2 = num1 % 10;
-    Console.WriteLine($"В числе 645, третье число {num2}");
+// //78 -> третьей цифры нет
+// int num = 78;
+// if (num/100 < 1){
+//     Console.WriteLine($"В числе {num}, третьей цифры нет");
+// }
+// else {
+// Console.WriteLine($"В числе {num}, третье число {(num/100)%10}");
+// }
 
-}
-
-Console.WriteLine($"В числе 645, третье число {num1}");
-//else Console.Write(num1);
-// int num1 = num % 100;
-// int num5 = num1 / 10;
-// int num2 = num / 100;
-// int num3 = num / 10;
-// //int num2 = num % 10;
-// //Console.Write(num1);
-// int num4 = num2 - num3;
-//Console.WriteLine(num1);
-// Console.WriteLine(num2);
-// Console.WriteLine(num3);
-// Console.WriteLine(num5);
-// Console.Write(num4);
+// //32679 -> 6
+// int num = 32679;
+// if (num/100 < 1){
+//     Console.WriteLine($"В числе {num}, третьей цифры нет");
+// }
+// else {
+// Console.WriteLine($"В числе {num}, третье число {(num/100)%10}");
+//}
